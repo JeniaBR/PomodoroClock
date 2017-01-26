0bf5793d5ba1b944212f42f1b917f1511a4dff0a
Jenia Brook <jeniabrook@gmail.com>
2017-01-26 19:03:15 +0200

    Added a timer and method to update countdown

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 $(document).ready(function () {
-    var sessionTime = 25;
+    var sessionTime = 1;
     var breakTime = 5;
-
+    var currentTime = Date.parse(new Date());
+    var deadline;
     // var clock = document.getElementsByClassName('big-clock');
     // var clockMinutes = clock.querySelector('.minutes');
     // var clockSeconds = clock.querySelector('.seconds');
+    deadline = new Date(currentTime + sessionTime * 60 * 1000);
+
+    function getRemainingTime(endTime) {
+        var time = Date.parse(endTime) - Date.parse(new Date());
+        var seconds = Math.floor((time / 1000) % 60);
+        var minutes = Math.floor((time / 1000 / 60) % 60);
+        return {
+            'total': time,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function updateClock() {
+        var t = getRemainingTime(deadline);
+
+        if (t.total<0) {
+            clearInterval(timeInterval);
+            t.minutes = 0;
+            t.seconds = 0;
+        }
+
+        $('#minutes').html(("0" + t.minutes).slice(-2));
+        $('#seconds').html(("0" + t.seconds).slice(-2));
+    }
 
     $('#break-num').html(("0" + breakTime).slice(-2));
     $('#session-num').html(("0" + sessionTime).slice(-2));
@@ -48,4 +74,7 @@ $(document).ready(function () {
         $('#session-num').html(("0" + sessionTime).slice(-2));
         $('#minutes').html(("0" + sessionTime).slice(-2));
     });
+
+    var timeInterval = setInterval(updateClock,1000);
+
 });
